da6700abe1fe1438ab1c9676a1ac91e1b314353b
benjamin huang <benjaminhuanghuang@gmail.com>
2022-02-24 11:06:19 -0800

    web server

--- server/src/main.rs
+++ server/src/main.rs
@@ -17,21 +17,45 @@ fn main() {
 
 fn handle_connection( mut stream: TcpStream){
   let mut buffer = [0; 1024];
-
   stream.read(&mut buffer).unwrap();
-  // Test1:
-  //println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
- 
-  //  Test2:
-  // let response = "HTTP/1.1 200 OK\r\n\r\n";
-  // stream.write(response.as_bytes()).unwrap();
-  // stream.flush().unwrap();
 
-  // 
+  let get = b"GET / HTTP/1.1\r\n";
+  let (status_line, filename) = 
+    if buffer.starts_with(get) {
+      ("HTTP/1.1 200 OK", "index.html")
+    }else{
+      ("HTTP/1.1 404 Not Found", "404.html")
+    };
+
+
+  let contents = fs::read_to_string(filename).unwrap();
+  let response = format!(
+    "{}\r\nContent-Length: {}\r\n\r\n{}",status_line,contents.len(), contents
+  );
+  stream.write(response.as_bytes()).unwrap();
+  stream.flush().unwrap();
+}
+
+
+/* handle_connection code snippets
+Test1:
+
+  println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+Test2:
+
+  let response = "HTTP/1.1 200 OK\r\n\r\n";
+  stream.write(response.as_bytes()).unwrap();
+  stream.flush().unwrap();
+
+Test3
   let contents = fs::read_to_string("index.html").unwrap();
   let response = format!(
     "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", contents.len(), contents
   );
   stream.write(response.as_bytes()).unwrap();
   stream.flush().unwrap();
-}
+
+
+
+*/
