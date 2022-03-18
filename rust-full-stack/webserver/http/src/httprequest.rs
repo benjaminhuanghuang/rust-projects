7e331dccd5498ef30ee9e62bd84e572264b97ceb
benjamin huang <benjaminhuanghuang@gmail.com>
2022-03-18 15:19:13 -0700

    web server

--- rust-full-stack/webserver/http/src/httprequest.rs
+++ rust-full-stack/webserver/http/src/httprequest.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug, PartialEq)]
 pub enum Method {
   Get,
@@ -34,7 +36,79 @@ impl From<&str> for Version {
   }
 }
 
+#[derive(Debug, PartialEq)]
+pub enum Resource {
+  Path(String),
+}
 
+#[derive(Debug)]
+pub struct HttpRequest {
+  pub method: Method,
+  pub version: Version,
+  pub resource: Resource,
+  pub headers: HashMap<String, String>,
+  pub msg_body: String,
+}
+
+impl From<String> for HttpRequest {
+  // 这里用String？
+  fn from(s: String) -> Self {
+    let mut parsed_method = Method::Uninitialized;
+    let mut parsed_version = Version::Uninitialized;
+    let mut parsed_resource = Resource::Path("".to_string());
+    let mut parsed_headers = HashMap::new();
+    let mut parsed_msg_body = "".to_string();
+
+    for line in req.lines() {
+      if line.contains("HTTP") {
+        let (method, resource, version) = process_req_line(line);
+        parsed_method = method;
+        parsed_resource = resource;
+        parsed_version = version;
+      } else if line.contains(":") {
+      } else if line.len() == 0 {
+      } else {
+        parsed_msg_body = line;
+      }
+    }
+
+    HttpRequest {
+      method: parsed_method,
+      version: parsed_version,
+      resource: parsed_resource,
+      headers: parsed_headers,
+      msg_body: parsed_msg_body.to_string(),
+    }
+  }
+}
+
+fn process_req_line(s: &str) -> (Method, Resource, Version) {
+  let mut words = s.split_whitespace();
+  let method = words.next().unwrap();
+  let resource = words.next().unwrap();
+  let version = words.next().unwrap();
+  (
+    method.into(),
+    Resource::Path(resource.to_string()),
+    version.into(),
+  )
+}
+
+fn process_header_line(s: &str) ->(String, String) {
+  let mut header_items = s.split(":");
+  let mut key = String::from("");
+  let mut value = String::from("");
+
+  if let Some(k) = header_items.next(){
+    key = k.to_string();
+  }
+  if let Some(v) = header_items.next(){
+    value = v.to_string()
+  }
+
+  (key, value)
+}
+//--------------------------------------------------
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -44,7 +118,6 @@ mod tests {
     let m: Method = "GET".into();
     assert_eq!(m, Method::Get)
   }
-
 
   #[test]
   fn test_version_into() {
