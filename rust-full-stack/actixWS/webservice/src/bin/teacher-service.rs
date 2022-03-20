b96356a0c828f096fb357061bb65d951645bad08
benjamin huang <benjaminhuanghuang@gmail.com>
2022-03-19 19:26:59 -0700

    update

--- rust-full-stack/actixWS/webservice/src/bin/teacher-service.rs
+++ rust-full-stack/actixWS/webservice/src/bin/teacher-service.rs
@@ -17,16 +17,16 @@ mod models;
 use routers::*;
 use state::AppState;
 
-
+      
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
   let shared_data = web::Data::new(AppState {
     health_check_response: "I'm OK.".to_string(),
     visit_count: Mutex::new(0), 
-    courses: Mutex::nex(vec![])
+    courses: Mutex::new(vec![])
   });
   
-  let app = move || App::new().app_data(shared_data.clone()).configure(general_routes);
+  let app = move || App::new().app_data(shared_data.clone()).configure(general_routes).configure(course_routes);
 
   HttpServer::new(app).bind("127.0.0.1:8964")?.run().await
 } 
--- rust-full-stack/actixWS/webservice/src/handlers.rs
+++ rust-full-stack/actixWS/webservice/src/handlers.rs
@@ -10,4 +10,34 @@ pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpRespons
 
   HttpResponse::Ok().json(&response)
 }
-  
+
+use super::models::Course;
+use chrono::Utc;
+
+pub async fn new_course(
+  new_course: web::Json<Course>,
+  app_state: web::Data<AppState>,
+) -> HttpResponse {
+  println!("Received new course");
+
+  let course_count = app_state
+    .courses
+    .lock()
+    .unwrap()
+    .clone()
+    .into_iter()
+    .filter(|course| course.teacher_id == new_course.teacher_id)
+    .collect::<Vec<Course>>()
+    .len();
+
+  let new_course = Course {
+    teacher_id: new_course.teacher_id,
+    id: Some(course_count + 1),
+    name: new_course.name.clone(),
+    time: Some(Utc::now().naive_utc()),
+  };
+
+  app_state.courses.lock().unwrap().push(new_course);
+
+  HttpResponse::Ok().json("Course added")
+}
