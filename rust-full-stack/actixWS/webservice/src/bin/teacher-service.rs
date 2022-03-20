c5bfc600059e5454751ec29105a36f62606e25b5
benjamin huang <benjaminhuanghuang@gmail.com>
2022-03-19 20:45:29 -0700

    add test

--- rust-full-stack/actixWS/webservice/src/bin/teacher-service.rs
+++ rust-full-stack/actixWS/webservice/src/bin/teacher-service.rs
@@ -17,17 +17,19 @@ mod models;
 use routers::*;
 use state::AppState;
 
-      
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
   let shared_data = web::Data::new(AppState {
     health_check_response: "I'm OK.".to_string(),
-    visit_count: Mutex::new(0), 
-    courses: Mutex::new(vec![])
+    visit_count: Mutex::new(0),
+    courses: Mutex::new(vec![]),
   });
-  
-  let app = move || App::new().app_data(shared_data.clone()).configure(general_routes).configure(course_routes);
 
+  let app = move || {
+    App::new()
+      .app_data(shared_data.clone())
+      .configure(general_routes)
+      .configure(course_routes)
+  };
   HttpServer::new(app).bind("127.0.0.1:8964")?.run().await
-} 
- 
+}
--- rust-full-stack/actixWS/webservice/src/handlers.rs
+++ rust-full-stack/actixWS/webservice/src/handlers.rs
@@ -42,6 +42,50 @@ pub async fn new_course(
   HttpResponse::Ok().json("Course added")
 }
 
+pub async fn get_courses_for_teacher(
+  app_state: web::Data<AppState>,
+  params: web::Path<(usize)>,
+) -> HttpResponse {
+  let teacher_id: usize = params.0;
+
+  let filtered_courses = app_state
+    .courses
+    .lock()
+    .unwrap()
+    .clone()
+    .into_iter()
+    .filter(|course| course.teacher_id == teacher_id)
+    .collect::<Vec<Course>>();
+
+  if filtered_courses.len() > 0 {
+    HttpResponse::Ok().json(filtered_courses)
+  } else {
+    HttpResponse::Ok().json("No courses found for teacher".to_string())
+  }
+}
+
+
+pub async fn get_course_detail(
+  app_state: web::Data<AppState>,
+  params: web::Path<(usize, usize)>,
+) -> HttpResponse {
+  let (teacher_id, course_id) = params.0;
+
+  let seleted_course = app_state
+    .courses
+    .lock()
+    .unwrap()
+    .clone()
+    .into_iter()
+    .find(|course| course.teacher_id == teacher_id && course.id == Some(course_id))
+    .ok_or("Course not found");
+
+  if let Ok(course) = seleted_course {
+    HttpResponse::Ok().json(course)
+  } else {
+    HttpResponse::Ok().json("Course not found".to_string())
+  }
+}
 
 #[cfg(test)]
 mod tests {
@@ -50,7 +94,7 @@ mod tests {
   use std::sync::Mutex;
 
   #[actix_rt::test]
-  async fn post_course_test(){
+  async fn post_course_test() {
     let course = web::Json(Course {
       teacher_id: 1,
       name: "Test course".into(),
@@ -66,6 +110,36 @@ mod tests {
 
     let resp = new_course(course, app_state).await;
     assert_eq!(resp.status(), StatusCode::OK);
+  }
 
+  #[actix_rt::test]
+  async fn get_all_course_susscess() {
+    let app_state: web::Data<AppState> = web::Data::new(AppState {
+      health_check_response: "".to_string(),
+      visit_count: Mutex::new(0),
+      courses: Mutex::new(vec![]),
+    });
+
+    let teacher_id: web::Path<(usize)> = web::Path::from((1));
+
+    let resp = get_courses_for_teacher(app_state, teacher_id).await;
+    
+    assert_eq!(resp.status(), StatusCode::OK);
+  }
+
+
+  #[actix_rt::test]
+  async fn get_one_cours_susscess() {
+    let app_state: web::Data<AppState> = web::Data::new(AppState {
+      health_check_response: "".to_string(),
+      visit_count: Mutex::new(0),
+      courses: Mutex::new(vec![]),
+    });
+
+    let params: web::Path<(usize, usize)> = web::Path::from((1, 1));
+
+    let resp = get_course_detail(app_state, params).await;
+    
+    assert_eq!(resp.status(), StatusCode::OK);
   }
 }
