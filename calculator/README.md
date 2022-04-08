df70598f80d620da29bd32ab1d2e76da1ede1a1e
benjamin huang <benjaminhuanghuang@gmail.com>
2022-04-08 13:30:46 -0700

    update

--- calculator/README.md
+++ calculator/README.md
@@ -1,4 +1,12 @@
-# Caluloatr 
- - Rust Programming Course for Beginners - Tutorial (https://www.youtube.com/watch?v=MsocPEZBd-M&ab_channel=freeCodeCamp.org)
+# CLI Caluloatr 
+ - Rust Programming Course for Beginners - Tutorial [2021]
+  - (https://www.youtube.com/watch?v=MsocPEZBd-M&ab_channel=freeCodeCamp.org)
 
  
+## args
+```
+```
+## Run 
+```
+  cargo run -- 1 + 2
+```
--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,5 +1,30 @@
-use std:: env ::{args, Args};
+use std::env::{args, Args};
 
 fn main() {
-    println!("Hello, world!");
+  let mut args: Args = args();
+
+  let first = args.nth(1).unwrap();
+  // nth() will call next() internally
+  let operator = args.nth(0).unwrap().chars().next().unwrap();
+  let second = args.nth(0).unwrap();
+  println!("{} {} {}", first, operator, second);
+
+  let first_num: f32 = first.parse::<f32>().unwrap();
+  let second_num: f32 = second.parse().unwrap();
+  let result = operate(operator, first_num, second_num);
+  println!("{:?}", output(first_num, operator, second_num, result));
+}
+
+fn operate(operator: char, first_num: f32, second_num: f32) -> f32 {
+  match operator {
+    '+' => first_num + second_num,
+    '-' => first_num - second_num,
+    '*' | 'x' | 'X' => first_num * second_num,
+    '/' => first_num / second_num,
+    _ => panic!("Invalid operator used.")
+  }
+}
+
+fn output(first_num: f32, operator: char, second_num: f32, result: f32) -> String {
+  format!("{} {} {} = {}", first_num, operator, second_num, result)
 }
