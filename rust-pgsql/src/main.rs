ac45d5083ea579973fa36a0f3b013931b876f569
benjamin huang <benjaminhuanghuang@gmail.com>
2022-04-19 01:38:50 -0700

    update

--- rust-pgsql/src/main.rs
+++ rust-pgsql/src/main.rs
@@ -9,10 +9,14 @@ use credentials::*;
 pub type Db = Pool<Postgres>;
 
 #[tokio::main]
-async fn main() {
-  let root_db = new_db_pool(PG_HOST, PG_ROOT_DB, PG_ROOT_USER, PG_ROOT_PWD, 1).await;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let db = new_db_pool(PG_HOST, PG_APP_DB, PG_APP_USER, PG_APP_PWD, 1).await?;
 
-  println!("{:?}", root_db);
+  let result = sqlx::query("SELECT * from todo").fetch_all(&db).await?;
+
+  assert_eq!(2, result.len());
+
+  Ok(())
 }
 
 async fn new_db_pool(
@@ -23,7 +27,7 @@ async fn new_db_pool(
   max_con: u32,
 ) -> Result<Db, sqlx::Error> {
   let con_string = format!("postgres://{}:{}@{}/{}", user, pwd, host, db);
-  println!("con string: {}", con_string);
+  //println!("con string: {}", con_string);
 
   PgPoolOptions::new()
     .max_connections(max_con)
