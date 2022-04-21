8221a70d699715f6d0a8402e50199934c6619151
benjamin huang <benjaminhuanghuang@gmail.com>
2022-04-21 13:37:20 -0700

    serde

--- rust-pgsql/src/main.rs
+++ rust-pgsql/src/main.rs
@@ -1,5 +1,6 @@
 use sqlx::postgres::PgPoolOptions;
 use sqlx::{Pool, Postgres};
+use serde::{Deserialize, Serialize};
 use std::time::Duration;
 
 mod credentials;
@@ -8,6 +9,14 @@ use credentials::*;
 
 pub type Db = Pool<Postgres>;
 
+
+#[derive(sqlx::FromRow, Debug, Clone, Serialize, Deserialize)]
+pub struct Todo {
+	pub id: i64,
+	pub cid: i64, // creator id
+	pub title: String
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
   let db = new_db_pool(PG_HOST, PG_APP_DB, PG_APP_USER, PG_APP_PWD, 1).await?;
@@ -15,6 +24,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   let result = sqlx::query("SELECT * from todo").fetch_all(&db).await?;
 
   assert_eq!(2, result.len());
+
+
+  let sql = "SELECT id, cid, title FROM todo ORDER BY id DESC LIMIT 1";
+  let query = sqlx::query_as::<_, Todo>(&sql);
+  let todos = query.fetch_all(&db).await?;
+
 
   Ok(())
 }
